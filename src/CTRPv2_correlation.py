0987e4f145678189beae6cdd4eb4bbd26d726642
Haeun Hwangbo <haeun0512@gmail.com>
2023-04-19 17:22:03 -0400

    separated experimental correlation to CTRPv2 and PDX

---
+++ src/CTRPv2_correlation.py
@@ -0,0 +1,204 @@
+import pandas as pd
+import numpy as np
+from itertools import combinations_with_replacement
+import yaml
+from collections.abc import Iterable
+from processing.precompute_CTRPv2_correlation import import_ctrp_data, precompute_moa_drug_list
+from utils import load_config
+
+_, DRUG_INFO, _, _ = import_ctrp_data()
+CHEMO_DRUGS, TARGETED_DRUGS = precompute_moa_drug_list(DRUG_INFO)
+
+
+def get_moa_drug_list(drug_list: Iterable[str], setname: str, chemo_drug_list=None, targeted_drug_list=None) -> np.array:
+    """Get drugs with setname MoA
+    
+    Args:
+        drug_list (Iterable[str]): list of drugs
+        setname (str): chemo or targeted
+
+    Returns:
+        np.array: drug list with given setname MoA.
+    """
+    assert setname in ['all', 'chemo', 'targeted']
+    
+    if setname == 'all':
+        return drug_list
+
+    if setname == 'chemo':
+        if chemo_drug_list is None:
+            _, drug_info, _, _ = import_ctrp_data()
+            drugs_with_moa, _ = precompute_moa_drug_list(drug_info)
+        else:
+            drugs_with_moa = chemo_drug_list
+    elif setname == 'targeted':
+        if targeted_drug_list is None:
+            _, drug_info, _, _ = import_ctrp_data()   
+            _, drugs_with_moa = precompute_moa_drug_list(drug_info)
+        else:
+            drugs_with_moa = targeted_drug_list
+
+    return np.intersect1d(drug_list, drugs_with_moa)
+    
+
+def prepare_ctrp_agg_data(data_dir: str, cancer_type='PanCancer', drugA_setname="all", drugB_setname="all") -> np.ndarray:
+    """Returns an array of correlation values of given drug pairs in the given cancer type
+
+    Args:
+        data_dir (str): directory where the precomputed correlation tables are stored.
+        cancer_type (str, optional): Cancer type of interest. Defaults to 'PanCancer'.
+        drugA_setname (str, optional): 'all', 'chemo', 'targeted', or specific drug. Defaults to "all".
+        drugB_setname (str, optional): 'all', 'chemo', or 'targeted'. Defaults to "all".
+
+    Returns:
+        np.ndarray: array of correlation values for given pairs
+    """
+
+    try:
+        df = pd.read_csv(f'{data_dir}/{cancer_type}_all_pairwise_correlation.csv',
+                        index_col=0, header=0)
+    except FileNotFoundError:
+        raise FileNotFoundError(
+            f"{cancer_type} cancer type not available. Please use PanCacner instead.")
+    
+    assert (drugA_setname in ['all', 'chemo', 'targeted']) or (drugA_setname in df.index)
+    assert drugB_setname in ['all', 'chemo', 'targeted']
+    df.astype(np.float64)
+    np.fill_diagonal(df.values, np.nan)  # convert diagonal values to NaN (correlation with itself)
+
+    # all vs. all -> return immediately
+    if drugA_setname == 'all' and drugB_setname == 'all':
+        vals = df.values.flatten()
+        vals = vals[~np.isnan(vals)]
+        return vals
+
+    set_vs_set_flag = (drugA_setname in ['all', 'chemo', 'targeted']) and (
+        drugB_setname in ['all', 'chemo', 'targeted'])
+    
+    # set vs. set
+    if set_vs_set_flag:
+        drugA_list = get_moa_drug_list(df.index, drugA_setname, chemo_drug_list=CHEMO_DRUGS, targeted_drug_list=TARGETED_DRUGS)
+        drugB_list = get_moa_drug_list(
+            df.index, drugB_setname, chemo_drug_list=CHEMO_DRUGS, targeted_drug_list=TARGETED_DRUGS)
+
+        vals = df.loc[drugA_list, drugB_list].values.flatten()
+        vals = vals[~np.isnan(vals)]
+        return vals
+    
+    # one vs. set
+    drugB_list = get_moa_drug_list(df.index, drugB_setname)
+    vals = df.loc[drugA_setname, drugB_list].values
+    vals = vals[~np.isnan(vals)]
+    return vals
+
+
+def calc_corr_dist_summary_stats(pairs: np.array) -> tuple[float, float, float, float, float]:
+    """Calaculate summary statstics (sample size, mean, std, quantiles) of given
+    correlation values.
+
+    Args:
+        pairs (np.array): 1D array of given correlation values
+
+    Returns:
+        float: sample size
+        float: mean of given correlation values
+        float: std of given correlation values
+        float: lower 95% of given correlation values
+        float: upper 95% of given correlation values
+    """
+    n = len(pairs)
+    avg = round(np.mean(pairs), 5)
+    std = round(np.std(pairs), 5)
+    lower = round(np.quantile(pairs, 0.025), 5)
+    upper = round(np.quantile(pairs, 0.975), 5)
+
+    return n, avg, std, lower, upper
+
+
+def get_CTRPv2_one_vs_one_correlation(data_dir: str, cancer_type: str, drugA: str, drugB: str) -> float:
+    """Get one drug vs. one drug correlation value from precomputed correlation data for CTRPv2
+
+    Args:
+        data_dir (str): directory where the precomputed correlation tables are stored.
+        cancer_type (str): Cancer type of interest
+        drugA (str): Harmonized Name of drug A (must be in CTRPv2 data)
+        drugB (str): Harmonized Name of drug A (must be in CTRPv2 data)
+
+    Raises:
+        FileNotFoundError: if correlation for the given cancer type could not be found
+
+    Returns:
+        float: pearsonr correlation coefficient
+    """
+    try:
+        df = pd.read_csv(f'{data_dir}/{cancer_type}_all_pairwise_correlation.csv',
+                        index_col=0, header=0)
+    except FileNotFoundError:
+        raise FileNotFoundError(
+            f"{cancer_type} cancer type not available. Please use PanCacner instead.")
+    
+    return round(df.loc[drugA, drugB], 5)
+
+
+def report_CTRPv2_correlation(data_dir: str, outfile: str):
+    cancer_types = ['PanCancer', 'Breast', 'Cervical', 'Colorectal', 'Gastric', 'HeadNeck', 'Leukemia',
+                    'Lung', 'Lymphoma', 'Melanoma', 'Myeloma', 'Ovarian', 'Pancreatic', 'Renal']
+    moas = ['all', 'targeted', 'chemo']
+    one_vs_sets = [('Breast', 'Lapatinib', 'all'), 
+                     ('Colorectal', 'Topotecan', 'targeted'),
+                     ('Gastric', 'Paclitaxel', 'chemo'),
+                     ('Gastric', 'Paclitaxel', 'targeted'),
+                     ('PanCancer', 'Docetaxel', 'chemo'),
+                     ('Leukemia', 'ibrutinib', 'all'),
+                     ('Leukemia', 'Idelalisib', 'targeted'),
+                     ('Lung', 'Gemcitabine', 'chemo'),
+                     ('Lung', 'Docetaxel', 'targeted'),
+                     ('Lung', 'Erlotinib', 'targeted'),
+                     ('Lung', 'Vincristine sulfate', 'chemo'),
+                     ('Ovarian', 'Gemcitabine', 'chemo')]
+    
+    one_vs_one = [('Breast', 'Gemcitabine', 'Paclitaxel'),
+                  ('Breast', 'Lapatinib', '5-Fluorouracil'),
+                  ('PanCancer', 'Topotecan', 'Oxaliplatin'),
+                  ('Leukemia', 'Navitoclax', 'Decitabine'),
+                  ('PanCancer', 'Methotrexate', 'Oxaliplatin'),
+                  ('Myeloma', 'Doxorubicin', 'Bortezomib'),
+                  ('Pancreatic', 'Erlotinib', 'Gemcitabine'),
+                  ('Pancreatic', 'Topotecan', '5-Fluorouracil'),
+                  ('Pancreatic', 'Paclitaxel', 'Gemcitabine')]
+    
+    with open(outfile, 'w') as fout:
+        fout.write('Cancer_type,drugA_set,drugB_set,n,mean,std,lower2.5%,upper97.5%\n')
+        for cancer in cancer_types:
+            for x, y in combinations_with_replacement(moas, 2):
+                try:
+                    pairwise_corr = prepare_ctrp_agg_data(data_dir, cancer, drugA_setname=x, drugB_setname=y)
+                    l, mean, std, lower, upper = calc_corr_dist_summary_stats(pairwise_corr)
+                    fout.write(f'{cancer},{x},{y},{l},{mean},{std},{lower},{upper}\n')
+                except FileNotFoundError:
+                    pass
+        for cancer, x, y in one_vs_sets:
+            try:
+                pairwise_corr = prepare_ctrp_agg_data(data_dir, cancer, drugA_setname=x, drugB_setname=y)
+                l, mean, std, lower, upper = calc_corr_dist_summary_stats(pairwise_corr)
+                fout.write(f'{cancer},{x},{y},{l},{mean},{std},{lower},{upper}\n')
+            except FileNotFoundError:
+                pass
+            except AssertionError:
+                print(cancer, x, y)
+        for cancer, x, y in one_vs_one:
+            r = get_CTRPv2_one_vs_one_correlation(data_dir, cancer, x, y)
+            fout.write(f'{cancer},{x},{y},1,{r},NA,NA,NA\n')
+
+
+def main():
+    config = load_config()
+
+    data_dir = config['cell_line']['data_dir']
+    table_dir = config['cell_line']['table_dir']
+
+    report_CTRPv2_correlation(data_dir, f'{table_dir}/CTRPv2_pairwise_correlation_distributions.csv')
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/PDXE_correlation.py
@@ -0,0 +1,84 @@
+import pandas as pd
+import numpy as np
+import yaml
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import pearsonr
+from collections.abc import Iterable
+from utils import load_config
+
+plt.style.use('env/publication.mplstyle')
+
+def get_pdx_corr_data(df: pd.DataFrame, model_info: pd.DataFrame,
+                      drug1: str, drug2: str, metric='BestAvgResponse'):
+    """Prepare data to calculate correlation between drug response to drug1 and 2.
+
+    Args:
+        df (pd.DataFrame): drug response data
+        model_info (pd.DataFrame): tumor type dataframe (model, tumor type info)
+        drug1 (str): name of drug 1
+        drug2 (str): name of drug 2
+        metric (str, optional): drug response metric. Defaults to 'BestAvgResponse'.
+
+    Returns:
+        pd.DataFrame: 
+    """
+    a = df[df['Treatment'] == drug1].set_index('Model')[metric].astype(float)
+    b = df[df['Treatment'] == drug2].set_index('Model')[metric].astype(float)
+    assert a.index.is_unique
+    assert b.index.is_unique
+    assert model_info.index.is_unique
+    merged = pd.concat([a, b, model_info], axis=1, join='inner')
+    merged.columns = [drug1, drug2] + list(model_info.columns)
+    return merged
+
+
+def draw_corr_pdx(processed_df):
+    """Plot scatterplot of two drug responses and calculate spearmanr correlation.
+
+    Args:
+        processed_df (pd.DataFrame): drug response data
+
+    Returns:
+        plt.figure: plotted figure
+    """
+    drug1 = processed_df.columns[0]
+    drug2 = processed_df.columns[1]
+    r, p = pearsonr(processed_df[drug1], processed_df[drug2])
+    fig, ax = plt.subplots(figsize=(3, 3))
+
+    drug1_inactive = (processed_df[drug1] >= 30).sum()
+    drug2_inactive = (processed_df[drug2] >= 30).sum()
+    total = processed_df.shape[0]
+    sns.scatterplot(x=drug1, y=drug2, data=processed_df, ax=ax)
+    ax.axvline(0, color='gray', linestyle='--')
+    ax.axhline(0, color='gray', linestyle='--')
+    if (drug1_inactive/total > 0.8) or (drug2_inactive/total > 0.8):
+        print("WARNING: at least one of the drug is inactive!")
+    ax.set_xlabel(f'{drug1} BestAvgResponse')
+    ax.set_ylabel(f'{drug2} BestAvgResponse')
+    ax.set_title('n={0} r={1:.2f}'.format(total, r))
+
+    return fig
+
+
+def main():
+    config = load_config()
+    data_dir = config['PDX']['data_dir']
+    fig_dir = config['PDX']['fig_dir']
+    
+    response = pd.read_csv(f'{data_dir}/PDXE_drug_response.csv', header=0)
+    model_info = pd.read_csv(f'{data_dir}/PDXE_model_info.csv', header=0, index_col=0)
+    df = get_pdx_corr_data(response, model_info, 'binimetinib', 'encorafenib')
+    fig = draw_corr_pdx(df[(df['Tumor Type'] == 'CM') & (df['BRAF_mut'] == 1)])
+    fig.savefig(f'{fig_dir}/CM-BRAFmut_binimetinib_encorafenib.pdf',
+                bbox_inches='tight', pad_inches=0.1)
+
+    df = get_pdx_corr_data(response, model_info, 'cetuximab', '5FU')
+    fig = draw_corr_pdx(df[(df['Tumor Type'] == 'CRC') & (df['RAS_mut'] == 0)])
+    fig.savefig(f'{fig_dir}/CRC-RASwt_cetuximab_5FU.pdf',
+                bbox_inches='tight', pad_inches=0.1)
+
+
+if __name__ == '__main__':
+    main()
