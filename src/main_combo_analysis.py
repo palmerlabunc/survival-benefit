2c390807c51fcc613f949db3c2f6da3625664350
Haeun Hwangbo <haeun0512@gmail.com>
2023-11-01 14:09:08 -0400

    now does analysis after running the algorithm

--- src/main_combo_analysis.py
+++ src/main_combo_analysis.py
@@ -3,93 +3,122 @@ from multiprocessing import Pool
 import shutil
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+import os
+import glob
 from survival_benefit.survival_benefit_class import SurvivalBenefit
 from utils import load_config
 
 
-def copy_if_not_exists(src_file: str, dest_file: str):
-    """Copy a file from src to dest if dest does not exist.
-    """
-    dest_path = Path(dest_file)
-    if not dest_path.exists():
-        try:
-            shutil.copy2(src_file, dest_file)
-        except FileNotFoundError:
-            print(f'File {src_file} does not exist.')
+def gini_index(values, tmax):
+    values = np.sort(values)
+    # cap the values at tmax
+    values[values > tmax] = tmax
+    n = len(values)
+    gini = (2 * np.sum(np.multiply(np.arange(1, n+1), values)) /
+            (n * np.sum(values))) - (n + 1) / n
+    return np.round(gini, 2)
 
+def endpoint_simple(endpoint: str) -> str:
+    if endpoint == 'OS':
+        return 'OS'
+    return 'Surrogate'
 
-def copy_trial_files_to_data_dir(config: dict):
-    """Copy the digitized KM curves and at-risk tables to the data directory.
-    """
-    data_dir = Path(config['data_dir'])
-    data_dir.mkdir(parents=True, exist_ok=True)
-    metadata_sheet = pd.read_csv(config['metadata_sheet'], header=0, index_col=None)
-
-    for _, row in metadata_sheet.iterrows():
-        raw_dir = row['Raw Path']
-        processed_dir = row['Processed Path']
-        control_prefix = row['Control']
-        combo_prefix = row['Combination']
-        atrisk_filename = SurvivalBenefit.get_atrisk_filename_from_comb_name(combo_prefix)
-        copy_if_not_exists(f'{raw_dir}/{control_prefix}.csv',
-                           f'{processed_dir}/{control_prefix}.csv')
-        copy_if_not_exists(f'{raw_dir}/{combo_prefix}.csv',
-                           f'{processed_dir}/{combo_prefix}.csv')
-        copy_if_not_exists(f'{raw_dir}/{atrisk_filename}',
-                           f'{processed_dir}/{atrisk_filename}')
-
-
-def compute_benefit_at_experimental_corr(data_dir: str, control_prefix: str, comb_prefix: str, 
-                                         corr: float, out_dir: str, n: int = 500):
-    """Compute the benefit at a given correlation.
-
-    Args:
-        data_dir (str): The directory of the data
-        control_prefix (str): The prefix of the control file
-        comb_prefix (str): The prefix of the combination file
-        corr (float): The correlation to compute the benefit
-        out_dir (str): The output directory
-        n (int, optional): The number of virtual patients to use. Defaults to 500.
-    """
-    postfix_corr = str(round(corr, 2))[2:] # e.g. 0.34 -> 34
-    survival_benefit = SurvivalBenefit(mono_name=f'{data_dir}/{control_prefix}', comb_name=f'{data_dir}/{comb_prefix}',
-                                       n_patients=n, outdir=out_dir, figsize=(4, 3))
+def compile_stats(table_dir: str, corr_type: str):
+    stat_df = pd.DataFrame()
     
-    survival_benefit.compute_benefit_at_corr(corr)
-    survival_benefit.plot_compute_benefit_sanity_check(save=True, postfix=f'.corr_{postfix_corr}')
-    survival_benefit.plot_t_delta_t_corr(
-        save=True, postfix=f'.corr_{postfix_corr}')
-    survival_benefit.plot_benefit_distribution(
-        save=True, postfix=f'.corr_{postfix_corr}')
-    survival_benefit.save_summary_stats(postfix=f'.corr_{postfix_corr}')
-    survival_benefit.save_benefit_df(postfix=f'.corr_{postfix_corr}')
+    for combo in os.listdir(table_dir):
+        dirname = os.path.join(table_dir, combo)
+        if not os.path.isdir(dirname):
+            continue
+        if len(os.listdir(dirname)) == 0:
+            continue
+        #FIXME this is a work-around. Make sure we used the specified target corr
+        print(dirname)
+
+        if corr_type == "high":
+            benefit_filename = glob.glob(
+                f'{dirname}/N_500.target_corr_1.00.*.table.csv')[0]
+            stats_filename = glob.glob(
+                f'{dirname}/N_500.target_corr_1.00.*.summary_stats.csv')[0]
+        else:
+            try:
+                benefit_filename = glob.glob(
+                    f'{dirname}/N_500.target_corr_0.*.table.csv')[0]
+            except IndexError:
+                benefit_filename = glob.glob(
+                    f'{dirname}/N_500.target_corr_-0.*.table.csv')[0]
+            try:
+                stats_filename = glob.glob(
+                    f'{dirname}/N_500.target_corr_0.*.summary_stats.csv')[0]
+            except IndexError:
+                stats_filename = glob.glob(
+                    f'{dirname}/N_500.target_corr_-0.*.summary_stats.csv')[0]
+        
+        stats = pd.read_csv(stats_filename,
+                            index_col=0, header=None).squeeze(axis=1)
+        benefit_df = pd.read_csv(
+            benefit_filename, index_col=0, header=0).squeeze(axis=1)
+
+        benefit_arr = benefit_df[benefit_df['valid']]['delta_t'].values
+        tmax = eval(stats['Max_time'])
+        stats.loc['gini_coefficient'] = gini_index(benefit_arr, tmax)
+        
+        combo = stats['Combination']
+        if 'VanCutsem2015' in combo:
+            tokens = combo.rsplit('_', 2)
+            stats.loc['combo_name'] = tokens[0]
+            stats.loc['endpoint'] = endpoint_simple(tokens[1])
+
+        else:
+            tokens = combo.rsplit('_', 1)
+            stats.loc['combo_name'] = tokens[0]
+            stats.loc['endpoint'] = endpoint_simple(tokens[1])
+        
+        stat_df = pd.concat([stat_df, stats], axis=1, join='outer')
+    
+    return stat_df.T
 
 
-def compute_benefit_at_corr_for_all_main_combo(config: dict, n_process: int = 4):
-    """Compute the benefit at a given correlation for all main combo in the input sheet.
-    
-    Args:
-        config (dict): The config dictionary of the dataset
-        n_process (int, optional): Number of processes to use. Defaults to 4.
-    """
-    input_sheet = pd.read_csv(
-        config['metadata_sheet'], header=0, index_col=None)
-    # create output directory
-    out_dir = config['table_dir']
-    Path(out_dir).mkdir(parents=True, exist_ok=True)
-    # prepare arguments
-    input_sheet.loc[:, 'Outdir'] = out_dir
-    args_arr = input_sheet[['Processed Path', 'Control', 'Combination', 'Corr', 'Outdir']].values
-    
-    with Pool(processes=n_process) as pool:
-        pool.starmap(compute_benefit_at_experimental_corr, args_arr)
+def plot_gini_curve(benefit_df: pd.DataFrame) -> plt.figure:
+    df = benefit_df[benefit_df['valid']]
+    cumsum = df['delta_t'].sort_values().cumsum().values
+    norm_cumsum = 100 * cumsum / cumsum[-1]
+    index = np.linspace(0, 100, len(norm_cumsum))
+
+    fig, ax = plt.subplots(figsize=(4, 4))
+    ax.plot(index, norm_cumsum)
+    ax.set_xlabel('Patients (%)')
+    ax.set_ylabel('Cumulative benefit (%)')
+    ax.set_ylim(0, 100)
+    ax.set_xlim(0, 100)
+    ax.plot([0, 100], [0, 100], linestyle='--', color='black')
+    return fig
+
+
+def plot_1mo_responder_percentage(data: pd.DataFrame) -> plt.figure:
+    sns.set_palette('deep')
+    fig, ax = plt.subplots(figsize=(4, 3))
+    sns.histplot(data['Percentage of patients benefitting 1mo'],
+                 binwidth=10, kde=True, ax=ax)
+    ax.set_xlim(0, 1)
+    return fig
 
 
 def main():
-    config = load_config()['main_combo']
-    copy_trial_files_to_data_dir(config)
-    compute_benefit_at_corr_for_all_main_combo(config, n_process=8)
+    config = load_config()
+    high_corr_stats = compile_stats(config['main_combo']['table_dir'], 'high')
+    exp_corr_stats = compile_stats(config['main_combo']['table_dir'], 'exp')
 
+    high_corr_stats.to_csv(f"{config['main_combo']['table_dir']}/high_corr_stats_compiled.csv",
+                           index=False)
+    exp_corr_stats.to_csv(f"{config['main_combo']['table_dir']}/exp_corr_stats_compiled.csv",
+                          index=False)
+    
 
 if __name__ == '__main__':
     main()
+
+
